be3accef7ee02a46dec352b1992bdb30686cf4eb
Mdsart <87175168+Mdsart@users.noreply.github.com>
2023-01-03 23:49:21 +0100

    Add files via upload

---
+++ fibonacci.cxx
@@ -0,0 +1,29 @@
+/**
+auteur: Maduka David <mdsartb@gmail.com>
+version: fibonacci python 3
+
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+/**programme qui affiche une suite fibonacci
+le resultat apres execution donnera une suite de:
+1, 2, 3, 5, 8, 13, 21, 34,  55
+**/
+
+int main()
+{
+    int i= 1, a=1, b=1, c=1;
+    while (i<10)
+    {
+         cout <<b<<endl;
+         b= a+c;
+         c=a;
+         a=b;
+         i= i+1;
+     }	
+}
